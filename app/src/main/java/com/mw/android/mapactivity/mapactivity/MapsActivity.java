bc056106ba2cef6ad68e1da975f27cf2136e0ae0
mananwason <manan13056@iiitd.ac.in>
2015-02-07 19:44:31 +0530

    Basic Map Done

--- app/src/main/java/com/mw/android/mapactivity/mapactivity/MapsActivity.java
+++ app/src/main/java/com/mw/android/mapactivity/mapactivity/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.mw.android.mapactivity.mapactivity;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,13 +9,21 @@ import android.widget.Toast;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     float zoomlevel;
+    Double[] points = {28.54435, 77.27272, 28.54704, 77.27359, 28.54754, 77.27390, 28.54804, 77.27332,
+            28.54747, 77.27290, 28.54754, 77.27353, 28.54695, 77.27327, 28.54619, 77.27330,
+            28.54395, 77.27248, 28.54420, 77.27071};
+
     LatLng centre = new LatLng(28.54435, 77.27272);
     LatLng girls_hostel = new LatLng(28.54704, 77.27359);
     LatLng boys_hostel = new LatLng(28.54754, 77.27390);
@@ -28,6 +37,7 @@ public class MapsActivity extends FragmentActivity {
         //setUpMapIfNeeded();
         createMapView();
         addMarker();
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 
     @Override
@@ -65,22 +75,55 @@ public class MapsActivity extends FragmentActivity {
 
     private void addMarker() {
         if (null != mMap) {
-            mMap.addMarker(new MarkerOptions()
-                            .position(centre)
-                            .title("Academic Building")
-                            .draggable(true)
-            );
-            mMap.addMarker(new MarkerOptions()
-                            .position(girls_hostel)
-                            .title("Girls Hostel")
-                            .draggable(true)
-            );
-            mMap.addMarker(new MarkerOptions()
-                            .position(boys_hostel)
-                            .title("Boys Hostel")
-                            .draggable(true)
-            );
+            HashMap<LatLng, String> hashMap = new HashMap<LatLng, String>();
+            hashMap.put(new LatLng(points[0], points[1]), "Academic Building");
+            hashMap.put(new LatLng(points[2], points[3]), "Girls Hostel");
+            hashMap.put(new LatLng(points[4], points[5]), "Boys Hostel");
+            hashMap.put(new LatLng(points[6], points[7]), "Tennis Court");
+            hashMap.put(new LatLng(points[8], points[9]), "Football Field");
+            hashMap.put(new LatLng(points[10], points[11]), "Badminton Court 1");
+            hashMap.put(new LatLng(points[12], points[13]), "Badminton Court 2");
+            hashMap.put(new LatLng(points[14], points[15]), "Student Centre");
+            hashMap.put(new LatLng(points[16], points[17]), "Library Building");
+            hashMap.put(new LatLng(points[18], points[19]), "Faculty Building");
+            /*hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+            hashMap.put(new LatLng(),"");
+*/
+            for (Map.Entry<LatLng, String> entry : hashMap.entrySet()) {
+                LatLng x = (LatLng) entry.getKey();
+                //String y =(LatLng)entry.getValue();
 
+                mMap.addMarker(new MarkerOptions()
+                                .position(x)
+                                .title(entry.getValue())
+                                .draggable(true)
+                );
+                mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                                                      @Override
+                                                      public void onInfoWindowClick(Marker marker) {
+
+                                                          if (marker.getTitle().equals("Student Centre")) {
+                                                              Toast.makeText(MapsActivity.this, marker.getTitle(), 1000).show();
+                                                              Intent student_ctr = new Intent(MapsActivity.this,Student_Centre.class);
+                                                              startActivity(student_ctr);
+
+                                                          }
+
+                                                      }
+                                                  }
+
+                );
+
+            }
         }
     }
 }
